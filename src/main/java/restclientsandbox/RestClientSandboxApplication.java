e8962c44342bc5f92c06f26b643e0f403162c5e9
BeksultanCoder <beksultancs@gmail.com>
2024-01-23 08:56:28 +0600

    Initial commit

---
+++ src/main/java/restclientsandbox/RestClientSandboxApplication.java
@@ -0,0 +1,13 @@
+package restclientsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestClientSandboxApplication.class, args);
+    }
+
+}
