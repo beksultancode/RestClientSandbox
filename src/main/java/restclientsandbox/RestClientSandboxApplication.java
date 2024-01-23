d8c00bacbca61f8889036e4753750f63196c6f12
BeksultanCoder <beksultancs@gmail.com>
2024-01-23 08:57:00 +0600

    Rest Client is amazing

--- src/main/java/restclientsandbox/RestClientSandboxApplication.java
+++ src/main/java/restclientsandbox/RestClientSandboxApplication.java
@@ -1,7 +1,10 @@
 package restclientsandbox;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class RestClientSandboxApplication {
@@ -10,4 +13,32 @@ public class RestClientSandboxApplication {
         SpringApplication.run(RestClientSandboxApplication.class, args);
     }
 
+    // https://jsonplaceholder.typicode.com/todos
+
+    @Bean
+    RestClient restClient(RestClient.Builder builder) {
+        return builder.baseUrl("https://jsonplaceholder.typicode.com").build();
+    }
+
+    @Bean
+    ApplicationRunner runner(RestClient restClient) {
+        return args -> {
+            Todo[] todos = restClient.get()
+                    .uri("/todos")
+                    .retrieve()
+                    .body(Todo[].class);
+
+            for (Todo todo : todos) {
+                System.out.println(todo);
+            }
+        };
+    }
+
+    record Todo(
+            Long id,
+            Long userId,
+            String title,
+            Boolean completed
+    ) {
+    }
 }
